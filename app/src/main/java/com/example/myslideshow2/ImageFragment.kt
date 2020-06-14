00fbb81a88fcf7fb219321c39b637a72f243148b
jotasato <arukanaika.aruka.jj@gmail.com>
2020-06-15 06:22:41 +0900

    ImageFragment.ktでクラスのインスタンス作成 argumentsに画像の格納から取り出し 取り出した画像をimageViewに表示

--- app/src/main/java/com/example/myslideshow2/ImageFragment.kt
+++ app/src/main/java/com/example/myslideshow2/ImageFragment.kt
@@ -5,29 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_image.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+/////////////////////////////////
+///クラスのインスタンスを作る処理///
+////////////////////////////////
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+val IMG_RES_ID = "IMG_RES_ID"
+
 class ImageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,22 +24,42 @@ class ImageFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        fun newInstance(imageResourceId: Int): ImageFragment {
+            val bundle = Bundle()
+            bundle.putInt(IMG_RES_ID, imageResourceId)
+            val imageFragment = ImageFragment()
+            imageFragment.arguments = bundle
+            return imageFragment
+        }
+    }
+
+//////////////////////////////
+///Bundleから値を取り出す処理///
+//////////////////////////////
+        //アーギュメンツから取り出した画像のリソースIDを保持する変数
+        private var imgResId: Int? = null
+        //フラグメントが作成された時、再生成された時onCreateが呼ばれる。
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            //letはkotlinのスコープ関数。アーギュメンツの中身はnullかもしれないので、安全呼び出し演算子?.とスコープ関数letを使って
+            //nullではないことを確認している。ここで取り出したBundleオブジェクトは、let関数内で暗黙の変数itでアクセスできる。
+            arguments?.let{
+                //アーギュメンツに保存されていた画像のリソースIDをimgResIdに保存している。
+                imgResId = it.getInt(IMG_RES_ID)
             }
+        }
+
+////////////////////////////////
+///ImageViewに画像を設定する処理//
+////////////////////////////////
+
+    //フラグメントのライフサイクルメソッドで、アクティビティのonCreateが完了した後に呼び出される。
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //imgResIdに保持しておいたリソースIDを使って、フラグメント内のImageViewに画像を設定している。
+        //imgResIdはnullの可能性があるので、?.とletを使ってnullではない場合のみ画像を表示するようにしている。
+        imgResId?.let{
+            imageView.setImageResource(it)
+        }
     }
 }
